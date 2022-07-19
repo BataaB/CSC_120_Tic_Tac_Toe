7695e440db1306e85323e90565333c781eec9da8
Batkhuleg B <khulegkhan@gmail.com>
2022-07-18 22:30:13 -0400

    Stage 1 of the project

---
+++ board.py
@@ -0,0 +1,12 @@
+board = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+]
+
+def print_board():
+    print("Printing board")
+    for row in board:
+        print(row)
+
+print_board()
